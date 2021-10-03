8b38a83ebbd9e279d1185543023d0746af638e67
Atafsmile <ansaritaufique027@gmail.com>
2021-10-04 00:48:21 +0530

    my first AngualarMaterialCard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,35 @@
 import { Component } from '@angular/core';
 
+
+
+export interface PeriodicElement {
+  iconname: any;
+  devloper: string;
+  weight: any;
+  buttons: any;
+  
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { devloper: 'web Flow Standar',
+  iconname: true, weight: 780, buttons: false },
+  { devloper: 'Web Flow Pro', iconname: false, 
+  weight: '1.7k', buttons: false },
+  { devloper: 'Bussiness Consulting', iconname: false,
+   weight: '120/hour', buttons: true },
+  { devloper: 'UI/UX Design Service', iconname: true,
+   weight: '120/hour', buttons: true },
+  { devloper: 'Product Testing', iconname: false, 
+  weight: '120/hour', buttons: true },
+  { devloper: 'Front End Development', iconname: true,
+   weight: '120/hour', buttons: true },
+  { devloper: 'Back End Devlopment', iconname: true,
+   weight: '120/hour', buttons: true },
+  { devloper: 'Service Design', iconname: true, 
+  weight: '120/hour', buttons: true }
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +37,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularmaterialBox';
-}
+  displayedColumns: string[] = ['devloper', 'iconname', 'weight', 'buttons'];
+  dataSource = ELEMENT_DATA;
+  
+
+  }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatBadgeModule} from '@angular/material/badge';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+      MatTableModule,
+      MatCheckboxModule,
+      MatPaginatorModule,
+      MatIconModule,
+      MatCardModule,
+      MatButtonModule,
+      MatGridListModule,
+      MatBadgeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
